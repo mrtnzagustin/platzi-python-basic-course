c16da926be3ed52f0479476117cd2ebfc60cb8c1
Agustin Martinez <mrtnz.agustin@gmail.com>
2021-04-20 00:13:12 +0000

    Basic example and conversor example with print and convertion values, etc.

---
+++ basic.py
@@ -0,0 +1,12 @@
+# EJEMPLO DE EDAD
+edad = int(input("Ingresar edad: "))
+
+if edad >= 18:
+    if edad >=65:
+        print("Sos mayor de edad y ademas adulto mayor.")
+    else:
+        print("Sos mayor de edad.")
+else:
+    print("Sos menor de edad.")
+
+
---
+++ conversor.py
@@ -0,0 +1,3 @@
+pesos_argentinos = int(input("Ingresar tus pesos argentinos: "))
+cotizacion_dolar = 97.5
+print("Tienes en total " + str(round(pesos_argentinos / cotizacion_dolar, 2)) + " dolares")
