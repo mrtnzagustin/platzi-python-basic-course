fa5948fb07fe9ec35806b84bddba108380784030
Agustin Martinez <mrtnz.agustin@gmail.com>
2021-04-20 00:14:21 +0000

    Palindrome function with standard main function.

---
+++ palindromo.py
@@ -0,0 +1,17 @@
+def es_palindromo(frase):
+    # Pasamos a minusculas y sacamos los espacios
+    frase = frase.lower().replace(" ","")
+    # Comparamos si la frase conincide con su inversa
+    return frase == frase[::-1]
+
+def run():
+    # Leemos la frase
+    frase = input("Ingrese una frase: ")
+    # Analizamos si la frases es igual a su inversa
+    if es_palindromo(frase):
+        print("Es palíndromo")
+    else:
+        print("No es palíndromo")
+
+if __name__ == "__main__":
+    run()
